93b1e474317e6bb7039953899fb6b95d56eb8499
Avinash Kumar <167669416+avinashkumar8473@users.noreply.github.com>
2026-06-05 20:37:47 +0530

    Add files via upload

---
+++ PrimeNo.java
@@ -0,0 +1,23 @@
+import java .util.*;
+public class PrimeNo {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num =sc.nextInt();
+        if(num<1){
+            System.out.println("number is not prime");
+            return;
+        }
+        int count=0;
+        for(int i=1;i<=num;i++){
+            if(num%i==0){
+                count++;
+            }
+        }
+        if(count==2){
+            System.out.println("number is prime");
+        }
+            else{
+            System.out.println("number is not prime");
+        }
+        }
+    }
